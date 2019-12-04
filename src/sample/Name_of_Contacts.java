f450a7b2653174c5787a157a0acbcf4ea1eb1e51
IbtashamAlee <aleejaan562@gmail.com>
2019-12-04 18:20:33 +0500

    New Class for getting name of contacts

--- src/sample/Name_of_Contacts.java
+++ src/sample/Name_of_Contacts.java
@@ -1,5 +1,20 @@
 package sample;
 
 public class Name_of_Contacts {
+    private String nameOfContacts;
 
+    public Name_of_Contacts (String nameOfContacts) {
+        this.nameOfContacts = nameOfContacts;
+    }
+
+    public Name_of_Contacts () {
+    }
+
+    public String getNameOfContacts () {
+        return nameOfContacts;
+    }
+
+    public void setNameOfContacts (String nameOfContacts) {
+        this.nameOfContacts = nameOfContacts;
+    }
 }
